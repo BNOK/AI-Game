6e810fd2fae0cd6e8787b5a5bb37685153b781b3
BNOK <52835202+BNOK@users.noreply.github.com>
2020-11-04 23:58:44 +0100

    FlappyBirdAI
    simple graphics mainly to develop the AI model

---
+++ FlappyBird/FlappyBird2D/Assets/Scripts/Bird.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class Bird : Agent
+{
+    private Rigidbody2D rb2D;
+    public float jumpForce = 500f;
+    private Vector2 jumper;
+    private Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = this.GetComponent<Rigidbody2D>();
+        jumper = new Vector2(0, jumpForce );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb2D.velocity = Vector2.zero;
+            rb2D.AddForce(jumper);
+        }
+    }
+
+    public override void Initialize()
+    {
+        rb2D = this.GetComponent<Rigidbody2D>();
+        jumper = new Vector2(0, jumpForce);
+        startPos = this.transform.position;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        this.transform.position = startPos;
+        rb2D.velocity = Vector2.zero;
+
+    }
+
+    
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        AddReward(0.1f);
+
+        if (vectorAction[0] == 1)
+        {
+            rb2D.velocity = Vector2.zero;
+            rb2D.AddForce(jumper);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AddReward(-1.0f);
+        EndEpisode();
+    }
+
+}
---
+++ FlappyBird/FlappyBird2D/Assets/Scripts/Mover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // obstacle parametres
+    public GameObject obstacle;
+    private Vector2 startPos = new Vector2(0,-100);
+    private GameObject[] obstList;
+    private int index=0;
+    
+
+    //scrolling speed 
+    public float speed = 20;
+
+    //spawn rate parametres
+    public int spawnRate= 3;
+    private float timer=0;
+   
+
+    //spawn parametres
+    private float yPos;
+    private float xPos = 60;
+    public float yMin=-20;
+    public float yMax=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstList = new GameObject[5];
+        
+        for (int i =0; i<5; i++)
+        {
+            obstList[i] = Instantiate(obstacle, startPos, Quaternion.identity);
+        }
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if ( timer>= spawnRate)
+        {
+            yPos = Random.Range(yMin, yMax);
+            obstList[index].transform.position = new Vector2(xPos, yPos);
+            index++;
+            timer = 0;
+        }
+        
+
+        if ( index == 5)
+        {
+            index = 0;
+        }
+
+        
+    }
+}
